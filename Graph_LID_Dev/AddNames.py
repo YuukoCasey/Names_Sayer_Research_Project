a3060b414b63ecf2871a1e351bbc3e1152937e79
Squark <squark@localhost.localdomain>
2019-07-02 21:37:30 +1200

    Added 203 samoan names from 'https://hearnames.com/pronunciations/samoan-names'

--- Graph_LID_Dev/AddNames.py
+++ Graph_LID_Dev/AddNames.py
@@ -13,12 +13,19 @@ while (True):
 	input_language = input(question_string)
 	input_language = input_language.title()
 
-	c.execute("INSERT INTO names(name, language) VALUES (?,?);", (input_name, input_language))
-	conn.commit()
+	c.execute("SELECT name FROM names WHERE name = ? AND language = ?;", (input_name, input_language))
+	query_result = c.fetchall()
 
-	user_answer = input("Would you like to add more names? ")
-	if (user_answer.lower() == "n" or user_answer.lower() == "no"):
-		break
+	if (len(query_result) == 0):
+		c.execute("INSERT INTO names(name, language) VALUES (?,?);", (input_name, input_language))
+		conn.commit()
+
+	else:
+		print("This name is already in this database under this language. Please select another name or language")
+
+#	user_answer = input("Would you like to add more names? ")
+#	if (user_answer.lower() == "n" or user_answer.lower() == "no"):
+#		break
 	print("\n\n\n\n\n")
 
 c.close()
