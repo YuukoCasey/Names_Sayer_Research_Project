64c00f4000e14e07a22cda1fd6a11c14a208518a
Squark <squark@localhost.localdomain>
2019-08-07 13:34:45 +1200

    The accuracy of identifying maori names seems to increase over time, but the accuracy with English names has almost reached a maximum. At what number of training names does the Maori name identification reach an accuracy of 90%

--- Graph_LID_Dev/CalculateAverageAccuracy.py
+++ Graph_LID_Dev/CalculateAverageAccuracy.py
@@ -4,21 +4,24 @@ import sqlite3
 conn = sqlite3.connect("TestingResults.db")
 c = conn.cursor()
 
-c.execute("SELECT AVG(Accuracy) FROM TestResults WHERE language = ?", ("English",))
-res = c.fetchall()
-res = res[0][0]
+for i in range(0, 99):
+	if (i%10 == 0 and i > 0):
+		c.execute("SELECT AVG(Accuracy) FROM TestResults WHERE language = ? AND trainingPercent=?", ("English",float(i)))
+		res = c.fetchall()
+		res = res[0][0]
+		print ("Average accuracy for ID'ing English names is " + str(res) + " when accuracy is " + str(i) + "%")
 
-print ("Average accuracy for ID'ing English names is " + str(res))
 
-c.execute("SELECT AVG(Accuracy) FROM TestResults WHERE language=?", ("Maori",))
-res = c.fetchall()
-res = res[0][0]
-print ("Average accuracy for ID'ing Maori names is " + str(res))
+		c.execute("SELECT AVG(Accuracy) FROM TestResults WHERE language=? AND trainingPercent=?", ("Maori",float(i)))
+		res = c.fetchall()
+		res = res[0][0]
+		print ("Average accuracy for ID'ing Maori names is " + str(res) + " when accuracy is " + str(i) + "%")
 
-c.execute("SELECT AVG(Accuracy) FROM TestResults WHERE language=?", ("Samoan",))
-res = c.fetchall()
-res = res[0][0]
-print ("Average accuracy for ID'ing Samoan names is " + str(res))
+
+		c.execute("SELECT AVG(Accuracy) FROM TestResults WHERE language=? AND trainingPercent=?", ("Samoan",float(i)))
+		res = c.fetchall()
+		res = res[0][0]
+		print ("Average accuracy for ID'ing Samoan names is " + str(res) + " when accuracy is " + str(i) + "%\n\n\n")
 
 c.close()
 conn.close()
